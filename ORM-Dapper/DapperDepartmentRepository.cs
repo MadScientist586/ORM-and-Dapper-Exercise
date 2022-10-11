3c208073857a2c26a16b6f5d4aa8d1d36a568837
MadScientist586 <mr.scientist.knight@gmail.com>
2022-10-11 11:01:44 -0500

    Finished exercise 1

---
+++ ORM-Dapper/DapperDepartmentRepository.cs
@@ -0,0 +1,32 @@
+﻿using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity.Core.Objects;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ORM_Dapper
+{
+    public class DapperDepartmentRepository : IDepartmentRepository
+    {
+        private readonly IDbConnection _conn;
+
+        public DapperDepartmentRepository(IDbConnection conn)
+        {
+            _conn = conn;
+        }
+
+        public IEnumerable<Department> GetAllDepartments()
+        {
+            return _conn.Query<Department>("SELECT * FROM departments");
+        }
+
+        public void InsertDepartment(string name)
+        {
+            _conn.Execute("INSERT INTO departments (name) VALUES (@name)", new {name = name});
+        }
+    }
+}
--- ORM-Dapper/Program.cs
+++ ORM-Dapper/Program.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿
+using Microsoft.Extensions.Configuration;
 using MySql.Data.MySqlClient;
 using System.Data;
 
@@ -9,13 +10,26 @@ namespace ORM_Dapper
         static void Main(string[] args)
         {
             var config = new ConfigurationBuilder()
-                  .SetBasePath(Directory.GetCurrentDirectory())
-                  .AddJsonFile("appsettings.json")
-                  .Build();
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
 
             string connString = config.GetConnectionString("DefaultConnection");
 
             IDbConnection conn = new MySqlConnection(connString);
+
+            var departmentRepo = new DapperDepartmentRepository(conn);
+
+            var departments = departmentRepo.GetAllDepartments();
+
+            foreach (var department in departments)
+            {
+                Console.WriteLine($"{department.DepartmentID}{department.Name}");
+                Console.WriteLine();
+                Console.WriteLine();
+
+            }
         }
     }
 }
+
